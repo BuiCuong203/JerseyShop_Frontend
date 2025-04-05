18654edf4f735180c3a3d77efd514c9ca4903120
Cuong <buimanhcuong2510@gmail.com>
2025-04-06 01:22:41 +0700

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import LastestCollection from '../components/LastestCollection'
+import BestSeller from '../components/BestSeller'
+
+const Home = () => {
+  return (
+    <div>
+      <Hero />
+      <LastestCollection />
+      <BestSeller />
+    </div>
+  )
+}
+
+export default Home
