528e7d213a6a2f107e6b7f077791d678be0ebc8d
Cuong <buimanhcuong2510@gmail.com>
2025-04-07 13:44:38 +0700

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LastestCollection from '../components/LastestCollection'
 import BestSeller from '../components/BestSeller'
+import OurPolicy from '../components/OurPolicy'
+import Newletterbox from '../components/Newletterbox'
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <Hero />
       <LastestCollection />
       <BestSeller />
+      <OurPolicy />
+      <Newletterbox />
     </div>
   )
 }
