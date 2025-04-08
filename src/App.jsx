1a68f22b6ef4edafc765ecfca8c888d16a4d00b5
Cuong <buimanhcuong2510@gmail.com>
2025-04-09 00:01:00 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,13 @@ import PlaceOrder from "./pages/PlaceOrder"
 import Orders from "./pages/Orders"
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
   return (
     <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
 
+      <ToastContainer />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,5 +1,6 @@
-import React, { createContext } from 'react'
+import React, { useState, createContext, useEffect } from 'react'
 import { products } from '../assets/frontend_assets/assets';
+import { toast } from 'react-toastify';
 
 export const ShopContext = createContext();
 
@@ -7,9 +8,44 @@ const ShopContextProvider = (props) => {
 
   const currency = 'đ';
   const delivery_fee = 10;
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId, size) => {
+    var cartData = structuredClone(cartItems);
+
+    if(!size){
+      toast.error("PLease select product size");
+    }
+
+    if(cartData[itemId]){
+      if(cartData[itemId][size]){
+        cartData[itemId][size] += 1;
+      }else{
+        cartData[itemId][size] = 1;
+      }
+    }else{
+      cartData[itemId] = {};
+      cartData[itemId][size] = 1;
+    }
+
+    setCartItems(cartData);
+  }
+
+  const getCartCount = () => {
+    let totalcount = 0;
+    for(const item in cartItems){
+      for(const size in cartItems[item]){
+        if(cartItems[item][size] && size){
+          totalcount += cartItems[item][size];
+        }
+      }
+    }
+    return totalcount;
+  }
 
   const value = {
-    products, currency, delivery_fee
+    products, currency, delivery_fee,
+    cartItems, addToCart, getCartCount
   }
 
   return (
