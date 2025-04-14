9932f714ab2324eee7267a04a3383da43cae78d6
Cuong <buimanhcuong2510@gmail.com>
2025-04-14 23:10:12 +0700

    first commit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,54 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import ReCAPTCHA from 'react-google-recaptcha';
 
 const Login = () => {
 
-  const [currentState, setCurrentState] = useState('Login');
+  const [currentState, setCurrentState] = useState('Đăng nhập');
+  const [captchaToken, setCaptchaToken] = useState(null);
+  const recaptchaRef = useRef(null);
 
   const onSubmitHandler = (e) => {
     e.preventDefault();
+
+    //Logic khi submit form gửi token lên server
+
+    recaptchaRef.current.reset();
+    setCaptchaToken(null);
   }
+
+  const onCaptchaChange = (token) => {
+    setCaptchaToken(token);
+  };
 
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
       <div className='inline-flex items-center gap-2 mb-2 mt-10'>
         <p className='prata-regular text-3xl'>{currentState}</p>
-        <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
       </div>
 
-      {currentState === 'Login' ? '' : <input className='w-full px-3 py-2 border border-gray-800' type="text" placeholder='Name' required />}
+      {currentState === 'Đăng nhập' ? '' : <input className='w-full px-3 py-2 border border-gray-800' type="text" placeholder='Tên người dùng' required />}
       <input className='w-full px-3 py-2 border border-gray-800' type="email" placeholder='Email' required />
-      <input className='w-full px-3 py-2 border border-gray-800' type="password" placeholder='Password' required />
+      <input className='w-full px-3 py-2 border border-gray-800' type="password" placeholder='Mật khẩu' required />
+      {currentState === 'Đăng nhập' ? '' : <input className='w-full px-3 py-2 border border-gray-800' type="password" placeholder='Nhập lại mật khẩu' required />}
+      {
+        currentState === 'Đăng nhập'
+        ? ''
+        : 
+        <div className='w-full'>
+          <ReCAPTCHA sitekey="6LcMuxUrAAAAAJUjH0M18y7ykDBtpBB6qEWl16-d" onChange={onCaptchaChange} ref={recaptchaRef} />
+        </div>
+      }
+
+      <button className='w-full bg-black text-white font-light px-8 py-2 cursor-pointer'>{currentState === 'Đăng nhập' ? 'Đăng nhập' : 'Đăng ký'}</button>
+      
       <div className='w-full flex justify-between text-sm mt-[-8px]'>
-        <p className='cursor-pointer'>Forgot your password?</p>
+        <p className='cursor-pointer'>Quên mật khẩu</p>
         {
-          currentState === 'Login'
-          ? <p onClick={() => setCurrentState('Sign Up')} className='cursor-pointer'>Create account</p>
-          : <p onClick={() =>  setCurrentState('Login')} className='cursor-pointer'>Login Here</p>
+          currentState === 'Đăng nhập'
+          ? <p onClick={() => setCurrentState('Đăng ký')} className='cursor-pointer'>Tạo tài khoản</p>
+          : <p onClick={() =>  setCurrentState('Đăng nhập')} className='cursor-pointer'>Đăng nhập ngay</p>
         }
       </div>
-
-      <button className='bg-black text-white font-light px-8 py-2 mt-4 cursor-pointer'>{currentState === 'Login' ? 'Sign In' : 'Sign Up'}</button>
     </form>
   )
 }
