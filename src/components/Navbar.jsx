6eb4f8f93bcee66e08b0b0c4405b8bd688fc3025
Cuong <buimanhcuong2510@gmail.com>
2025-04-20 01:04:29 +0700

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -55,7 +55,7 @@ const Navbar = () => {
                 <img onClick={() => setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt='' />
             </div>
 
-            <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
+            <div className={`absolute top-0 right-0 bottom-0 z-20 overflow-hidden bg-white transition-all ${visible ? 'w-full': 'w-0'}`}>
                 <div className='flex flex-col text-gray-600 cursor-pointer'>
                     <div  onClick={() => setVisible(false)} className='flex items-center gap-4 p-3'>
                         <img className='h-4 rotate-180' src={assets.dropdown_icon} alt='' />
